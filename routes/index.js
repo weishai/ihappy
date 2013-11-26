b06b701cf1f99335aa31c018b350c892ea403241
weishai <masterkingr9@gmail.com>
2013-11-26 20:51:09 +0800

    update upload

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@
  * GET home page.
  */
 
+var upload = require('jquery-file-upload-middleware')
+  , fs = require('fs')
+
 module.exports = function(app) {
   app.get('/', function (req, res) {
     res.render('index');
@@ -13,17 +16,33 @@ module.exports = function(app) {
   });
 
   app.post('/uploadfile', function (req, res) {
-    var upload = require('jquery-file-upload-middleware');
-    upload.configure({
-        uploadDir: __dirname + '/public/uploads',
-        uploadUrl: '/uploads',
-        imageVersions: {
-            thumbnail: {
-                width: 80,
-                height: 80
-            }
-        }
-    });
-    upload.fileHandler();
+    var userFiles = req.files.csslift.length > 1 ? req.files.csslift : [req.files.csslift]
+    // for(var i in userFiles){
+    //   if (userFiles[i].size == 0){
+    //     // 使用同步方式删除一个文件
+    //     fs.unlinkSync(userFiles[i].path);
+    //     console.log('Successfully removed an empty file!');
+    //   } else {
+    //     var target_path = './public/uploads/csslift/' + userFiles[i].name;
+    //     console.log(userFiles[i].name);
+    //     // 使用同步方式重命名一个文件
+    //     fs.renameSync(userFiles[i].path, target_path);
+    //     console.log('Successfully renamed a file!');
+    //   }
+    // }
+    // res.json(userFiles)
+    // fs.readFile(userFiles[0].path, function (err, contents) {
+    //   res.write(contents)
+    //   res.end
+    // })
+
+    // res.writeHead(302, {
+    //   'Location': 'http://localhost:3000/showHtml'
+    // });
+    // res.end();
+  });
+
+  app.get('/showHtml', function (req, res) {
+    res.send('good showHtml');
   });
 };
