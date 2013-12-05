cd238384f58f7ad217d645d679958271d2401449
weishai <masterkingr9@gmail.com>
2013-12-05 20:49:48 +0800

    up

--- public/js/sea-modules/ihappy/create.js
+++ public/js/sea-modules/ihappy/create.js
@@ -1,3 +1,30 @@
 define(function(require, exports, module) {
-  
+  console.log('create.js module start')
+  var $ = require('jquery')
+
+  var createBox = function (element, options) {
+    this.$element = element
+    this.$btnCreate = options.btnCreate
+    this.bindEvent()
+  }
+  createBox.prototype.open = function () {
+    $('#topNav').addClass('nav-hide')
+    this.$element.show(function () {
+      $(this).addClass('create-box-active')
+    })
+  },
+  createBox.prototype.close =function () {
+
+  },
+  createBox.prototype.bindEvent = function () {
+    var self = this
+    this.$btnCreate.on('click', function(e) {
+      self.open()
+      return false
+    })
+  }
+
+  var ihappyCreate = new createBox($('#createBox'), {
+    btnCreate: $('#btnCreate')
+  })
 })
