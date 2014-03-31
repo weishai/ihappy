ea23ae754da957d79ec9989c81a6b1a1e6ee4c23
weishai <masterkingr9@gmail.com>
2014-04-01 02:40:54 +0800

    update grunts

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,13 +18,40 @@ module.exports = function(grunt) {
           }
         ]
       }
+    },
+
+    less: {
+      development: {
+        options: {
+          // compress: true,
+          // cleancss: true,
+          optimization: 2
+        },
+        files: {
+          // target.css file: source.less file
+          "public/css/<%= pkg.name %>-debug.css": "public/less/<%= pkg.name %>.less"
+        }
+      }
+    },
+
+    watch: {
+      styles: {
+        // Which files to watch (all .less files recursively in the less directory)
+        files: ['public/less/**/*.less'],
+        tasks: ['less'],
+        options: {
+          spawn: false
+        }
+      }
     }
   });
 
-  // 加载包含 "uglify" 任务的插件。
+  // 加载插件。
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // 默认被执行的任务列表。
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['watch']);
 
 };
